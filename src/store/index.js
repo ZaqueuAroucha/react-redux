a12d4d781dd628470e0ed5fcc7a2d5f1484af15a
zaqueu aroucha da silva <zaqueuaroucha@gmail.com>
2022-10-12 20:35:58 -0300

    Subindo projeto da aula React-redux

---
+++ src/store/index.js
@@ -0,0 +1,35 @@
+import { createStore } from 'redux'
+// REDUCER
+
+const INITIAL_STATE = {
+  tasks: [
+    'Estudar React.js'
+  ]
+}
+
+
+function todo(state = INITIAL_STATE, action) {
+  switch(action.type) {
+    case 'ADD_TASK':
+      return {
+        ...state,
+        tasks: [
+          ...state.tasks,
+          action.payload
+        ]
+      }
+
+    case 'REMOVE_TASK':
+      const newState = state.tasks.filter(task => task !== action.payload)
+      return {
+        tasks: newState
+      }
+      
+    default:
+      return state
+  }
+}
+
+const store = createStore(todo)
+
+export default store
